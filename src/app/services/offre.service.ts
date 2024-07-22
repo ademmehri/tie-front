23da4dc152a69f0b4584c836dfda2f4344b6313b
mehriadem309@gmail.com <mehriadem309@gmail.com>
2024-07-22 10:23:03 +0200

    taw taw

--- src/app/services/offre.service.ts
+++ src/app/services/offre.service.ts
@@ -9,7 +9,7 @@ import { Offre } from '../models/listoffre.model';
 })
 export class OffreService {
 
-  private url="https://www.tie-job.com:8080"
+  private url="https://www.tie-job.com:8080/api"
 
   constructor(private httpclt:HttpClient,private route:Router) {}
   getItem(){
